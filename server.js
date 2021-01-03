c7bcb6a6f86b1982dc6b87025272a5ad5e3402b1
Milanrdnvc <milanradinovic@rocketmail.com>
2021-01-03 15:39:24 +0100

    Initialized MongoDB

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (_, res) => {
-  const test = [
-    { id: '1', firstName: 'Milan', lastName: 'Radinovic' },
-    { id: '2', firstName: 'John', lastName: 'Doe' },
-    { id: '3', firstName: 'Steve', lastName: 'Smith' },
-  ];
-  res.json(test);
-});
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+mongoose.connect(
+  process.env.MONGODB_CONNECTION_STRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  err => {
+    if (err) throw err;
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  }
+);
