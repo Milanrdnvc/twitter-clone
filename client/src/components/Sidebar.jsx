1327191fc4cc71f0c254e4a6f442e0e059d56755
Milan <milanradinovic@rocketmail.com>
2025-04-06 10:33:52 +0200

    Add assets and components solder

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { FaTwitter, FaHome } from "react-icons/fa";
+import Home from "@mui/icons-material/Home";
+import Notifications from "@mui/icons-material/Notifications";
+import Logout from "@mui/icons-material/Logout";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <FaTwitter />
+
+      <div className="sidebar-option active">
+        <Home />
+        <h2>Home</h2>
+      </div>
+
+      <div className="sidebar-option">
+        <Notifications />
+        <h2>Notifications</h2>
+      </div>
+
+      <div className="sidebar-option">
+        <Logout />
+        <h2>Logout</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
