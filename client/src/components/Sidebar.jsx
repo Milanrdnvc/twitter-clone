f367e0e3139d9fb43521c62d9bd8bc7f35ae4d73
Milan <milanradinovic@rocketmail.com>
2025-04-12 10:00:46 +0200

    Update sidebar to use <Link> instead of <a>

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FaTwitter } from "react-icons/fa";
 import { RiHome7Fill } from "react-icons/ri";
 import { IoIosNotifications } from "react-icons/io";
@@ -7,20 +8,25 @@ import { IoLogOut } from "react-icons/io5";
 function Sidebar() {
   return (
     <aside className="w-[250px] p-4 border-r border-gray-800 hidden lg:flex flex-col">
-      <FaTwitter className="text-3xl mb-4 text-pink-500 cursor-pointer" />
+      <Link to="/">
+        <FaTwitter className="text-3xl mb-4 text-pink-500 cursor-pointer" />
+      </Link>
       <nav className="flex flex-col gap-4">
-        <a href="#" className="flex items-center gap-3 hover:text-pink-500">
+        <Link to="/" className="flex items-center gap-3 hover:text-pink-500">
           <RiHome7Fill className="text-3xl relative top-[2px] text-pink-500" />
           <span className="text-base font-bold">Home</span>
-        </a>
-        <a href="#" className="flex items-center gap-3 hover:text-pink-500">
+        </Link>
+        <Link to="#" className="flex items-center gap-3 hover:text-pink-500">
           <IoIosNotifications className="text-3xl relative top-[2px] right-[2px] text-pink-500" />
           <span className="text-base font-bold">Notifications</span>
-        </a>
-        <a href="#" className="flex items-center gap-3 hover:text-pink-500">
+        </Link>
+        <Link
+          to="/login"
+          className="flex items-center gap-3 hover:text-pink-500"
+        >
           <IoLogOut className="text-3xl relative top-[2px] left-[1.2px] text-pink-500" />
           <span className="text-base font-bold">Log Out</span>
-        </a>
+        </Link>
       </nav>
     </aside>
   );
