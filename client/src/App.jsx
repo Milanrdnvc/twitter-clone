617fa04cc01ff8c07966e98f1ed4b2633ab32465
Milan <milanradinovic@rocketmail.com>
2025-04-13 10:14:11 +0200

    Add toastify for popups about errors etc.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
-import HomePage from "./pages/HomePage";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./styles/styles.css";
 
 function App() {
-  return <Outlet />;
+  return (
+    <>
+      <ToastContainer />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- client/src/pages/LogInPage.jsx
+++ client/src/pages/LogInPage.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useLoginMutation } from "../slices/usersApiSlice";
 import { setCredentials } from "../slices/authSlice";
+import { toast } from "react-toastify";
 
 function LogIn() {
   const [email, setEmail] = useState("");
@@ -35,7 +36,7 @@ function LogIn() {
       );
       navigate("/");
     } catch (err) {
-      console.log(err?.data?.message || err.error);
+      toast.error(err?.data?.message || err.error);
     }
   }
 
