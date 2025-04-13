92521fec74aa3f9aabf1e0b6ad750c28fba72c70
Milan <milanradinovic@rocketmail.com>
2025-04-13 10:06:28 +0200

    Set up redux toolkit for state management and implement login

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import store from "./store.js";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
@@ -22,7 +24,9 @@ const router = createBrowserRouter(
 );
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </Provider>
 );
--- client/src/pages/LogInPage.jsx
+++ client/src/pages/LogInPage.jsx
@@ -1,7 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useLoginMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
 
 function LogIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [login, { isLoading, error }] = useLoginMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate("/");
+    }
+  }, [navigate, userInfo]);
+
+  async function submitHandler(e) {
+    e.preventDefault();
+    try {
+      const res = await login({
+        email,
+        password,
+      }).unwrap();
+      dispatch(
+        setCredentials({
+          ...res,
+        })
+      );
+      navigate("/");
+    } catch (err) {
+      console.log(err?.data?.message || err.error);
+    }
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-[var(--color-bg)] text-[var(--color-primary)] px-4">
       <div className="bg-[var(--color-bg-secondary)] p-8 rounded-xl shadow-xl w-full max-w-md">
@@ -9,16 +46,18 @@ function LogIn() {
           Log In
         </h1>
 
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={submitHandler}>
           <input
             type="email"
             placeholder="Email"
             className="bg-[var(--color-bg-tertiary)] text-white px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-500"
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="password"
             placeholder="Password"
             className="bg-[var(--color-bg-tertiary)] text-white px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-500"
+            onChange={(e) => setPassword(e.target.value)}
           />
 
           <button
