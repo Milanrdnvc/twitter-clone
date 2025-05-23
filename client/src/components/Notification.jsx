58181abf57ad8b4b7860a8a2a431c9830ffa564d
Milan <milanradinovic@rocketmail.com>
2025-05-23 14:20:13 +0200

    Add Notification component

---
+++ client/src/components/Notification.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Notification({ info }) {
+  return <li className="p-3 bg-[#253341] rounded">{info}</li>;
+}
+
+export default Notification;
--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -1,23 +1,17 @@
 import React from "react";
+import Notification from "./Notification";
 
 function Notifications({ open, onClose }) {
   if (!open) return null;
 
   return (
     <div className="fixed inset-0 bg-black/70 z-50 flex items-center justify-center">
-      <div
-        className="bg-[#192734] p-6 rounded-lg shadow-lg w-full max-w-md"
-        onClick={(e) => e.stopPropagation()}
-      >
+      <div className="bg-[#192734] p-6 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-xl font-bold mb-4">Notifications</h2>
         <ul className="space-y-3">
-          <li className="p-3 bg-[#253341] rounded">
-            📢 You have a new follower!
-          </li>
-          <li className="p-3 bg-[#253341] rounded">
-            ❤️ Someone liked your tuwueet!
-          </li>
-          <li className="p-3 bg-[#253341] rounded">💬 You got a reply!</li>
+          <Notification info={`📢 You have a new follower!`} />
+          <Notification info={` ❤️ Someone liked your tuwueet!`} />
+          <Notification info={`💬 You got a reply!!`} />
         </ul>
         <button
           onClick={onClose}
